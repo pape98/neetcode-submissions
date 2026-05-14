43f102a778f71f7112f1d4bcecd42db0ee68367d
Pape Sow Traoré <papisline2222@gmail.com>
2026-05-14 11:25:14 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+
+    // T: O(n) where n is length of string
+    countFrequencies(str){
+        const freq = Array(26).fill(0);
+        for(const char of str){
+            const index = char.charCodeAt(0) - 97
+            freq[index] += 1;
+        }
+
+        return freq;
+    }
+
+    groupAnagrams(strs) {
+        const map = {};
+        
+        // T: O(mn) S: O(m)
+        for(const str of strs){
+            const freq = this.countFrequencies(str);
+            if(!map[freq]) map[freq] = [str];
+            else map[freq].push(str);
+        }
+        return Object.values(map);
+    }
+}
